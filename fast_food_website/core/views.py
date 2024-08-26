10cf1172873a88879673e1a9964b098acfe84247
SulemanAwais <sulemanawais2001@gmail.com>
2024-08-26 07:00:21 +0500

    🎉 Set up landing page route and template

--- fast_food_website/core/views.py
+++ fast_food_website/core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+
+def landing_page(request):
+    return render(request, 'core/landing_page.html')
